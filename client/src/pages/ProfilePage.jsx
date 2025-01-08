fc46c4a55702409a901f2ad3433562186e5d3099
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-01-08 14:03:51 +0300

    edit profile bg

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -40,9 +40,9 @@ const ProfilePage = () => {
     }
 
     return (
-        <div className="relative flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-lg shadow-gray-200">
-                <div className="relative flex items-center rounded-t-lg bg-gray-200 py-20 justify-center space-x-4">
+        <div className="relative flex items-center justify-center min-h-screen max-sm:bg-white bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg max-sm:shadow-none shadow-lg shadow-gray-200">
+                <div className="relative flex items-center rounded-t-lg bg-gradient-to-r from-[#FFF1DB]  to-[#D4BDAC] py-20 justify-center space-x-4">
                     <Avatar
                         size={90}
                         name={user.name}
@@ -54,12 +54,12 @@ const ProfilePage = () => {
                 <div className="text-center !mt-14">
                     <h2 className="text-xl font-medium text-gray-900">{user.name}</h2>
                     <p className="text-md text-gray-400">@{user.username}</p>
-                    <p className="text-sm text-gray-700">{user.email}</p>
+                    <p className="text-md text-gray-700">{user.email}</p>
                 </div>
                 <div className="flex items-center justify-center">
                     <button
                         onClick={handleLogout}
-                        className="w-1/2 flex items-center cursor-pointer justify-center font-medium text-sm py-3 px-4 bg-gradient-to-r from-[#0074b4] to-[#00b34c] text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                        className="w-1/2 flex items-center cursor-pointer justify-center font-medium text-md py-3 px-4 bg-gradient-to-r from-[#0074b4] to-[#00b34c] text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                     >
                         Logout
                     </button>
