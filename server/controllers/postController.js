b16c0de5564210435663c668d52360e6c8e75ec8
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-01-08 17:33:11 +0300

    edit cache control

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -5,7 +5,7 @@ const createPost = async (req, res) => {
     const userId = req.user._id;
     const username = req.user.username;
     const name = req.user.name;
-    
+
     try {
         const post = new Post({ user: userId, username, name, content });
         await post.save();
@@ -18,6 +18,7 @@ const createPost = async (req, res) => {
 const getPosts = async (req, res) => {
     try {
         const posts = await Post.find().populate("user", "username name").sort({ createdAt: -1 });
+        res.setHeader("Cache-Control", "no-cache");
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({ message: "Postlar getirilemedi", error });
