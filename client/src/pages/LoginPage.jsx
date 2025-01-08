30ffbd64fb7775f8a34c67a6db7bd00bd4c88ef1
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-01-08 14:43:18 +0300

    edit

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
             setSuccess(response.data.message);
             setTimeout(() => {
                 setLoading(false);
-                navigate("/posts");
+                navigate("/profile");
             }, 1000);
         } catch (error) {
             setError(error.response?.data?.message || "Error logging in user");
