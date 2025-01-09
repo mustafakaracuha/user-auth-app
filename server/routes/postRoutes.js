85b5cc8df4aabef5ce1685a97d0c37b2a9ac9043
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-01-09 10:49:25 +0300

    edit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createPost, getPosts } = require("../controllers/postController");
+const { createPost, getPosts, getTotalPosts } = require("../controllers/postController");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
